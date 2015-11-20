03a2c2b632379a9caf9d8b1c3eeb6e3ca12adee7
Najam Ahmed Ansari <ahmed.najamansari@gmail.com>
2015-11-20 17:40:01 +0500

    Mock data for the IIIF interface

--- features/mock_data.py
+++ features/mock_data.py
@@ -2,3 +2,23 @@
 """
 from textwrap import dedent
 import simplejson
+
+iiif_info = {
+    'profile': [
+        'http://iiif.io/api/image/2/level1.json',
+        {
+            'supports': [
+                'regionByPct', 'sizeByForcedWh', 'sizeByWh',
+                'profileLinkHeader', 'jsonldMediaType'
+            ],
+            'qualities': ['color', 'gray'],
+            'formats': ['png']
+        }
+    ],
+    'tiles': [{'width': 1000, 'scaleFactors': [4], 'height': 1000}],
+    'protocol': 'http://iiif.io/api/image',
+    'height': 1080,
+    'width': 1920,
+    '@context': 'http://iiif.io/api/image/2/context.json',
+    '@id': 'http://iiif-host.org/iiif/image'
+}
